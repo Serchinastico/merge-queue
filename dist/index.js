c6d76cf59d581fd0c4263b053148874d1b86e469
Serchinastico <54cymru@gmail.com>
2020-10-24 16:22:10 +0200

    CLean up

--- dist/index.js
+++ dist/index.js
@@ -69,7 +69,6 @@ const getInput = () => {
 };
 const isEventInBaseBranch = (context) => {
     const isFromPullRequest = !!context.payload.pull_request;
-    console.log(JSON.stringify(context.payload));
     return !isFromPullRequest;
 };
 const fireNextPullRequestUpdate = (context, input, octokit) => __awaiter(void 0, void 0, void 0, function* () {
@@ -100,8 +99,58 @@ const fireNextPullRequestUpdate = (context, input, octokit) => __awaiter(void 0,
         console.log(`There is no next Pull Request in line.`);
     }
 });
-const run = () => __awaiter(void 0, void 0, void 0, function* () {
+const mergePullRequestIfPossible = (context, input, octokit) => __awaiter(void 0, void 0, void 0, function* () {
     var _c;
+    const payload = context.payload;
+    const labels = payload.pull_request.labels;
+    const hasReadyToMergeLabel = labels.some((label) => label.name === input.mergeLabelName);
+    if (!hasReadyToMergeLabel) {
+        console.log(`Pull Request does not have the "${ansi_colors_1.default.bold.blue(input.mergeLabelName)}" label.`);
+        return;
+    }
+    const pullRequestId = {
+        owner: (_c = payload.repository.owner.name) !== null && _c !== void 0 ? _c : payload.repository.owner.login,
+        repo: payload.repository.name,
+        pull_number: payload.pull_request.number,
+    };
+    const pullRequest = yield octokit.pulls.get(pullRequestId);
+    if (pullRequest.data.state !== 'open') {
+        console.log('Pull Request is not open. Cannot merge it.');
+        return;
+    }
+    if (pullRequest.data.draft) {
+        console.log('Pull Request is in draft. Cannot merge it.');
+        return;
+    }
+    if (!pullRequest.data.mergeable) {
+        console.log("Pull Request can't be merged.");
+        return;
+    }
+    // Pull Request is out of date and we should update it
+    if (pullRequest.data.mergeable_state === 'behind') {
+        console.log('Pull Request is outdated.');
+        // See if it's next in line
+        const allPullRequests = yield octokit.pulls.list({
+            owner: pullRequestId.owner,
+            repo: pullRequestId.repo,
+            state: 'open',
+            base: input.baseBranchName,
+            sort: 'created',
+            direction: 'asc',
+        });
+        const firstPullRequestInQueue = allPullRequests.data.find((pr) => pr.labels.find((label) => label.name === input.mergeLabelName));
+        if ((firstPullRequestInQueue === null || firstPullRequestInQueue === void 0 ? void 0 : firstPullRequestInQueue.id) !== pullRequest.data.id) {
+            console.log('Pull Request is not next in line. Waiting for other Pull Request to be merged first.');
+            return;
+        }
+        console.log('Updating Pull Request.');
+        yield octokit.pulls.updateBranch(pullRequestId);
+        return;
+    }
+    console.log('Pull Request is about to be merged.');
+    yield octokit.pulls.merge(Object.assign(Object.assign({}, pullRequestId), { merge_method: input.mergeMethod }));
+});
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const context = github.context;
         const input = getInput();
@@ -110,63 +159,7 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
             yield fireNextPullRequestUpdate(context, input, octokit);
         }
         else {
-            const payload = context.payload;
-            const labels = payload.pull_request.labels;
-            const hasReadyToMergeLabel = labels.find((label) => label.name === input.mergeLabelName);
-            if (!hasReadyToMergeLabel) {
-                console.log(`Pull Request does not have the "${input.mergeLabelName}" label. Unable to merge`);
-                return;
-            }
-            const pullRequestId = {
-                owner: (_c = payload.repository.owner.name) !== null && _c !== void 0 ? _c : payload.repository.owner.login,
-                repo: payload.repository.name,
-                pull_number: payload.pull_request.number,
-            };
-            console.log('Payload');
-            console.log(payload);
-            console.log(pullRequestId);
-            const pullRequest = yield octokit.pulls.get(pullRequestId);
-            console.log('Pull Request');
-            console.log(pullRequest);
-            if (pullRequest.data.state !== 'open') {
-                console.log('Pull Request is not open. Cannot merge it.');
-                return;
-            }
-            if (pullRequest.data.draft) {
-                console.log('Pull Request is in draft. Cannot merge it.');
-                return;
-            }
-            if (!pullRequest.data.mergeable) {
-                console.log("Pull Request can't be merged.");
-                return;
-            }
-            // Pull Request is out of date and we should update it
-            if (pullRequest.data.mergeable_state === 'behind') {
-                console.log('Pull Request is outdated.');
-                // See if it's next in line
-                const allPullRequests = yield octokit.pulls.list({
-                    owner: pullRequestId.owner,
-                    repo: pullRequestId.repo,
-                    state: 'open',
-                    base: input.baseBranchName,
-                    sort: 'created',
-                    direction: 'asc',
-                });
-                const firstPullRequestInQueue = allPullRequests.data.find((pr) => pr.labels.find((label) => label.name === input.mergeLabelName));
-                console.log('First pull request is:');
-                console.log(firstPullRequestInQueue);
-                console.log(firstPullRequestInQueue === null || firstPullRequestInQueue === void 0 ? void 0 : firstPullRequestInQueue.id);
-                console.log(pullRequest.data.id);
-                if ((firstPullRequestInQueue === null || firstPullRequestInQueue === void 0 ? void 0 : firstPullRequestInQueue.id) !== pullRequest.data.id) {
-                    console.log('Pull Request is not next in line. Waiting for other Pull Request to be merged first.');
-                    return;
-                }
-                console.log('Updating Pull Request.');
-                yield octokit.pulls.updateBranch(pullRequestId);
-                return;
-            }
-            console.log('Pull Request is about to be merged.');
-            yield octokit.pulls.merge(Object.assign(Object.assign({}, pullRequestId), { merge_method: input.mergeMethod }));
+            yield mergePullRequestIfPossible(context, input, octokit);
         }
     }
     catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,6 @@ const getInput = (): Input => {
 const isEventInBaseBranch = (context: Context) => {
   const isFromPullRequest = !!context.payload.pull_request
 
-  console.log(JSON.stringify(context.payload))
   return !isFromPullRequest
 }
 
@@ -93,6 +92,86 @@ const fireNextPullRequestUpdate = async (
   }
 }
 
+const mergePullRequestIfPossible = async (
+  context: Context,
+  input: Input,
+  octokit: Octokit
+) => {
+  const payload = context.payload as Webhooks.EventPayloads.WebhookPayloadPullRequest
+  const labels = payload.pull_request.labels
+
+  const hasReadyToMergeLabel = labels.some(
+    (label) => label.name === input.mergeLabelName
+  )
+  if (!hasReadyToMergeLabel) {
+    console.log(
+      `Pull Request does not have the "${c.bold.blue(
+        input.mergeLabelName
+      )}" label.`
+    )
+    return
+  }
+
+  const pullRequestId = {
+    owner: payload.repository.owner.name ?? payload.repository.owner.login,
+    repo: payload.repository.name,
+    pull_number: payload.pull_request.number,
+  }
+
+  const pullRequest = await octokit.pulls.get(pullRequestId)
+
+  if (pullRequest.data.state !== 'open') {
+    console.log('Pull Request is not open. Cannot merge it.')
+    return
+  }
+
+  if (pullRequest.data.draft) {
+    console.log('Pull Request is in draft. Cannot merge it.')
+    return
+  }
+
+  if (!pullRequest.data.mergeable) {
+    console.log("Pull Request can't be merged.")
+    return
+  }
+
+  // Pull Request is out of date and we should update it
+  if (pullRequest.data.mergeable_state === 'behind') {
+    console.log('Pull Request is outdated.')
+
+    // See if it's next in line
+    const allPullRequests = await octokit.pulls.list({
+      owner: pullRequestId.owner,
+      repo: pullRequestId.repo,
+      state: 'open',
+      base: input.baseBranchName,
+      sort: 'created',
+      direction: 'asc',
+    })
+
+    const firstPullRequestInQueue = allPullRequests.data.find((pr) =>
+      pr.labels.find((label) => label.name === input.mergeLabelName)
+    )
+
+    if (firstPullRequestInQueue?.id !== pullRequest.data.id) {
+      console.log(
+        'Pull Request is not next in line. Waiting for other Pull Request to be merged first.'
+      )
+      return
+    }
+
+    console.log('Updating Pull Request.')
+    await octokit.pulls.updateBranch(pullRequestId)
+    return
+  }
+
+  console.log('Pull Request is about to be merged.')
+  await octokit.pulls.merge({
+    ...pullRequestId,
+    merge_method: input.mergeMethod,
+  })
+}
+
 const run = async (): Promise<void> => {
   try {
     const context = github.context
@@ -102,88 +181,7 @@ const run = async (): Promise<void> => {
     if (isEventInBaseBranch(context)) {
       await fireNextPullRequestUpdate(context, input, octokit)
     } else {
-      const payload = context.payload as Webhooks.EventPayloads.WebhookPayloadPullRequest
-      const labels = payload.pull_request.labels
-
-      const hasReadyToMergeLabel = labels.find(
-        (label) => label.name === input.mergeLabelName
-      )
-      if (!hasReadyToMergeLabel) {
-        console.log(
-          `Pull Request does not have the "${input.mergeLabelName}" label. Unable to merge`
-        )
-        return
-      }
-
-      const pullRequestId = {
-        owner: payload.repository.owner.name ?? payload.repository.owner.login,
-        repo: payload.repository.name,
-        pull_number: payload.pull_request.number,
-      }
-      console.log('Payload')
-      console.log(payload)
-      console.log(pullRequestId)
-
-      const pullRequest = await octokit.pulls.get(pullRequestId)
-
-      console.log('Pull Request')
-      console.log(pullRequest)
-
-      if (pullRequest.data.state !== 'open') {
-        console.log('Pull Request is not open. Cannot merge it.')
-        return
-      }
-
-      if (pullRequest.data.draft) {
-        console.log('Pull Request is in draft. Cannot merge it.')
-        return
-      }
-
-      if (!pullRequest.data.mergeable) {
-        console.log("Pull Request can't be merged.")
-        return
-      }
-
-      // Pull Request is out of date and we should update it
-      if (pullRequest.data.mergeable_state === 'behind') {
-        console.log('Pull Request is outdated.')
-
-        // See if it's next in line
-        const allPullRequests = await octokit.pulls.list({
-          owner: pullRequestId.owner,
-          repo: pullRequestId.repo,
-          state: 'open',
-          base: input.baseBranchName,
-          sort: 'created',
-          direction: 'asc',
-        })
-
-        const firstPullRequestInQueue = allPullRequests.data.find((pr) =>
-          pr.labels.find((label) => label.name === input.mergeLabelName)
-        )
-
-        console.log('First pull request is:')
-        console.log(firstPullRequestInQueue)
-        console.log(firstPullRequestInQueue?.id)
-        console.log(pullRequest.data.id)
-
-        if (firstPullRequestInQueue?.id !== pullRequest.data.id) {
-          console.log(
-            'Pull Request is not next in line. Waiting for other Pull Request to be merged first.'
-          )
-          return
-        }
-
-        console.log('Updating Pull Request.')
-        await octokit.pulls.updateBranch(pullRequestId)
-        return
-      }
-
-      console.log('Pull Request is about to be merged.')
-      await octokit.pulls.merge({
-        ...pullRequestId,
-        merge_method: input.mergeMethod,
-      })
+      await mergePullRequestIfPossible(context, input, octokit)
     }
   } catch (error) {
     core.setFailed(error.message)
